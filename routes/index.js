861f78ae428c59c20053123c0a8051aa1f8916ad
CateRios <caterinarios@gmail.com>
2020-10-18 20:41:40 +0100

    Esqueleto del proyecto con express-generator y página de bienvenida

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TING 2020' });
+  res.render('index', { title: 'RING-TING-TING' });
 });
 
 module.exports = router;
