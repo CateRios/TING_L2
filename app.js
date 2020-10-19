daa663ed7d5a2c820ff47293a04f88379d86852f
CateRios <caterinarios@gmail.com>
2020-10-19 22:26:37 +0100

    Nueva ruta

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 //Mis routers
-
+var serviciosRouter = require('./routes/servicios');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //Mis rutas
-
+app.use('/servicios', serviciosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
