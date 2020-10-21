58fc86ffa7a561c316ceac4492e9c9173f12b65b
CateRios <caterinarios@gmail.com>
2020-10-21 22:55:08 +0100

    creación de la vista base de datos

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 
 //Mis routers
 var serviciosRouter = require('./routes/servicios');
+var dbRouter = require('./routes/db');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 
 //Mis rutas
 app.use('/servicios', serviciosRouter);
+app.use('/db', dbRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
