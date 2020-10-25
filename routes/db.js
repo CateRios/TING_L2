67ee5e8e47715931b09291b7da2611e89462338e
CateRios <caterinarios@gmail.com>
2020-10-25 17:46:06 +0000

    prueba

--- routes/db.js
+++ routes/db.js
@@ -16,6 +16,7 @@ router.get('/', async(req, res) => {
         const client = await pool.connect();
         const result = await pool.query('SELECT * FROM users_table ORDER BY id DESC');
         const results = { 'results': (result) ? result.rows : null};
+        results = JSON.parse(results);
         res.render('db',{title: 'RING-TING-TING', results});
         client.release();
     }catch (err){
