60a05bba4dfd34e377a748cf8e5a943ecfaa9204
CateRios <caterinarios@gmail.com>
2020-10-25 17:52:50 +0000

    coreccion de errores

--- routes/db.js
+++ routes/db.js
@@ -5,7 +5,6 @@ const pool = new Pool({
         rejectUnauthorized: false
     }
 });
-const rp = require("request-promise");
 
 var express = require('express');
 var router = express.Router();
@@ -17,9 +16,6 @@ router.get('/', async(req, res) => {
         const client = await pool.connect();
         const result = await pool.query('SELECT * FROM users_table ORDER BY id DESC');
         const results = { 'results': (result) ? result.rows : null};
-        await new Promise((resolve) => {
-            setTimeout(resolve, 1000);
-          });
         res.render('db',{title: 'RING-TING-TING', results});
         client.release();
     }catch (err){
